c3bd383f9946e3000b82dfc7532cb53c3582956a
psq <psq@wumis.com>
2021-07-29 15:57:38 +0800

    psq测试

--- admin/src/main/java/com/website/admin/Application.java
+++ admin/src/main/java/com/website/admin/Application.java
@@ -17,9 +17,6 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan("com.website.mapper")
 @ComponentScan("com.website")
 public class Application {
-     void test(){
-         System.out.println("3213");
-     }
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
